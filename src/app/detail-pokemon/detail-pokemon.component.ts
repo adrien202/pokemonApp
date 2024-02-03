1faefe45825ed2cea0207d4c84718521a3a75c78
Adrien202 <adrien.abescat@gmail.com>
2024-02-03 10:17:06 +0100

    add routing

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {Pokemon} from "../model/pokemon";
+import {POKEMONS} from "../model/mock-pokemon";
 
 @Component({
   selector: 'app-detail-pokemon',
@@ -8,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailPokemonComponent implements OnInit {
 
-  constructor() { }
+  pokemonList: Pokemon[];
+  pokemon:Pokemon|undefined
+
+  constructor(private route:ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.pokemonList = POKEMONS;
+    const pokemonId = this.route.snapshot.paramMap.get('id');
+    if (pokemonId !== null) {
+      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    }
   }
 
+  goToPokemonList() {
+    this.router.navigate(['/pokemons'])
+  }
 }
